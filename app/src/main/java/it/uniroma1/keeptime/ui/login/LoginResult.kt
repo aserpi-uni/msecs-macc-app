563a6ff90d051b7daa866fa245ccbb9a30ab8e5d
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-03-07 14:34:39 +0100

    Initialise app

---
+++ app/src/main/java/it/uniroma1/keeptime/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package it.uniroma1.keeptime.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)

40d6c68dba2205919af978d0e0836eb118e1d25c
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-03-18 00:19:18 +0100

    Update email sign-in validator

--- app/src/main/java/it/uniroma1/keeptime/ui/login/LoginViewModel.kt
+++ app/src/main/java/it/uniroma1/keeptime/ui/login/LoginViewModel.kt
@@ -42,11 +42,7 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
 
     // A placeholder username validation check
     private fun isUserNameValid(username: String): Boolean {
-        return if (username.contains('@')) {
-            Patterns.EMAIL_ADDRESS.matcher(username).matches()
-        } else {
-            username.isNotBlank()
-        }
+        return Patterns.EMAIL_ADDRESS.matcher(username).matches()
     }
 
     // A placeholder password validation check

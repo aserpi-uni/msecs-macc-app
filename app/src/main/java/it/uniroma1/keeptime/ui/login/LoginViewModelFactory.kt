7547c43fb7cdeebfd7784ee25611d37edd9a7fc7
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-03-18 16:12:16 +0100

    Remove LoginDataSource

--- app/src/main/java/it/uniroma1/keeptime/ui/login/LoginViewModelFactory.kt
+++ app/src/main/java/it/uniroma1/keeptime/ui/login/LoginViewModelFactory.kt
@@ -2,7 +2,6 @@ package it.uniroma1.keeptime.ui.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import it.uniroma1.keeptime.data.LoginDataSource
 import it.uniroma1.keeptime.data.LoginRepository
 
 /**
@@ -15,9 +14,7 @@ class LoginViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
             return LoginViewModel(
-                    loginRepository = LoginRepository(
-                            dataSource = LoginDataSource()
-                    )
+                    loginRepository = LoginRepository()
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")

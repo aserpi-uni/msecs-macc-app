301281ea111486bdd43e9fadfee609f4769b8a72
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-03-18 16:46:05 +0100

    Update LoggedInUser with real attributes

--- app/src/main/java/it/uniroma1/keeptime/data/model/LoggedInUser.kt
+++ app/src/main/java/it/uniroma1/keeptime/data/model/LoggedInUser.kt
@@ -4,6 +4,6 @@ package it.uniroma1.keeptime.data.model
  * Data class that captures user information for logged in users retrieved from LoginRepository
  */
 data class LoggedInUser(
-        val userId: String,
-        val displayName: String
+        val email: String,
+        val url: String
 )

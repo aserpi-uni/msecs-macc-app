3ead0f80bfffd40faab8f3141a22b49063e4c0cf
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-03-18 15:38:56 +0100

    Add Volley

---
+++ app/src/main/java/it/uniroma1/keeptime/data/NetworkRequestSingleton.kt
@@ -0,0 +1,28 @@
+package it.uniroma1.keeptime.data
+
+import android.content.Context
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
+
+class NetworkRequestSingleton constructor(context: Context) {
+    companion object {
+        @Volatile
+        private var INSTANCE: NetworkRequestSingleton? = null
+        fun getInstance(context: Context) =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: NetworkRequestSingleton(context).also {
+                    INSTANCE = it
+                }
+            }
+    }
+
+    val requestQueue: RequestQueue by lazy {
+        // applicationContext is key, it keeps you from leaking the
+        // Activity or BroadcastReceiver if someone passes one in.
+        Volley.newRequestQueue(context.applicationContext)
+    }
+    fun <T> addToRequestQueue(req: Request<T>) {
+        requestQueue.add(req)
+    }
+}

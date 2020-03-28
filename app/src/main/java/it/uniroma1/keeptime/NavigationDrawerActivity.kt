9b52181c6520e5edf2fabd4c12a74d6030585a54
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-03-28 19:42:43 +0100

    Remove FAB

--- app/src/main/java/it/uniroma1/keeptime/NavigationDrawerActivity.kt
+++ app/src/main/java/it/uniroma1/keeptime/NavigationDrawerActivity.kt
@@ -1,8 +1,6 @@
 package it.uniroma1.keeptime
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -25,11 +23,6 @@ class NavigationDrawerActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)

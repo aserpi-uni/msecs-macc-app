e80cfdd6c3150ea3dee726e8047e12b16d4d4421
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-04-01 00:34:22 +0200

    Retrieve workspace after navigation

--- app/src/main/java/it/uniroma1/keeptime/ui/workspace/WorkspaceFragment.kt
+++ app/src/main/java/it/uniroma1/keeptime/ui/workspace/WorkspaceFragment.kt
@@ -1,20 +1,46 @@
 package it.uniroma1.keeptime.ui.workspace
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.serialization.json.Json
 
 import it.uniroma1.keeptime.R
+import it.uniroma1.keeptime.data.model.WorkspaceReference
+import it.uniroma1.keeptime.databinding.WorkspaceBinding
+import it.uniroma1.keeptime.ui.base.BaseFragment
+import it.uniroma1.keeptime.ui.modals.ModalBottomSheet
 
-class WorkspaceFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreate(savedInstanceState)
-        return inflater.inflate(R.layout.workspace, container, false)
+        workspaceViewModel = ViewModelProvider(this).get(WorkspaceViewModel::class.java)
+        viewModel = workspaceViewModel
+
+        val binding = DataBindingUtil.inflate<WorkspaceBinding>(
+            inflater,
+            R.layout.workspace,
+            container,
+            false
+        )
+        val view = binding.root
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = workspaceViewModel
+
+        workspaceViewModel.message.observe(viewLifecycleOwner, Observer {
+            val message = it ?: return@Observer
+            Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show()
+        })
+
+        setHasOptionsMenu(true)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -23,5 +49,7 @@ class WorkspaceFragment : Fragment() {
         val navView: BottomNavigationView = view.findViewById(R.id.workspace_bottom_nav)
         val navController = Navigation.findNavController(view.findViewById(R.id.workspace_host_fragment))
         navView.setupWithNavController(navController)
+
+        workspaceViewModel.getWorkspace(Json.parse(WorkspaceReference.serializer(), args.workspaceJson))
     }
 }
--- app/src/main/java/it/uniroma1/keeptime/ui/workspace/WorkspaceViewModel.kt
+++ app/src/main/java/it/uniroma1/keeptime/ui/workspace/WorkspaceViewModel.kt
@@ -1,4 +1,40 @@
 package it.uniroma1.keeptime.ui.workspace
 
-class WorkspaceViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.android.volley.*
+import it.uniroma1.keeptime.R
+import it.uniroma1.keeptime.data.model.Workspace
+import it.uniroma1.keeptime.data.model.WorkspaceReference
+import it.uniroma1.keeptime.ui.base.BaseViewModel
+import kotlinx.coroutines.launch
+
+class WorkspaceViewModel : BaseViewModel() {
+    private val _busy = MutableLiveData<Boolean>()
+    val busy: LiveData<Boolean> = _busy
+
+    private val _message = MutableLiveData<Int>()
+    val message: LiveData<Int> = _message
+
+    private val _workspace = MutableLiveData<Workspace>()
+    val workspace: LiveData<Workspace> = _workspace
+
+    fun getWorkspace(reference: WorkspaceReference) = viewModelScope.launch {
+        try {
+            _busy.value = true
+            _workspace.value = reference.fromServer()
+        } catch (error: AuthFailureError) {
+            _logoutMessage.value = R.string.failed_wrong_credentials
+        } catch (error: VolleyError) {
+            _message.value = when(error) {
+                is NoConnectionError, is TimeoutError -> R.string.failed_no_response
+                is NetworkError -> R.string.failed_network
+                is ParseError, is ServerError -> R.string.failed_server
+                else -> R.string.failed_unknown
+            }
+        } finally {
+            _busy.value = false
+        }
+    }
 }

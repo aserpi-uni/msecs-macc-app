7da0375d2d446a33f1bf10bcf2430cad8ddab195
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-04-01 11:23:50 +0200

    Remove unused workers viewmodel

--- app/src/main/java/it/uniroma1/keeptime/ui/workers/WorkersFragment.kt
+++ app/src/main/java/it/uniroma1/keeptime/ui/workers/WorkersFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -22,13 +21,11 @@ class WorkersFragment : Fragment() {
     private lateinit var workspaceViewModel: WorkspaceViewModel
     private lateinit var workersAdapter: WorkersAdapter
     private lateinit var workersLayoutManager: LinearLayoutManager
-    private lateinit var workersViewModel: WorkersViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         workspaceViewModel = ((parentFragment as NavHostFragment).parentFragment as WorkspaceFragment).workspaceViewModel
         workersAdapter = WorkersAdapter(workspaceViewModel.workspace.value!!.workers)
         workersLayoutManager = LinearLayoutManager(context)
-        workersViewModel = ViewModelProvider(this).get(WorkersViewModel::class.java)
 
         return inflater.inflate(R.layout.workers, container, false)
     }
--- app/src/main/java/it/uniroma1/keeptime/ui/workers/WorkersViewModel.kt
+++
@@ -1,5 +0,0 @@
-package it.uniroma1.keeptime.ui.workers
-
-import androidx.lifecycle.ViewModel
-
-class WorkersViewModel : ViewModel()

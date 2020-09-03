6bcb4aff2fce3ce9436a64384900687e010edeca
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-09-03 11:11:04 +0200

    Add Project classes

---
+++ app/src/main/java/it/uniroma1/keeptime/data/model/Project.kt
@@ -0,0 +1,83 @@
+package it.uniroma1.keeptime.data.model
+
+import android.net.Uri
+import it.uniroma1.keeptime.data.DateSerializer
+import it.uniroma1.keeptime.data.UriSerializer
+import kotlinx.serialization.*
+import java.util.*
+
+
+/**
+ * Class for projects.
+ */
+@Serializable
+open class Project(
+    val deliveryTime: Date,
+    val description: String,
+    projectName: String,
+    val status: String,
+    url: Uri,
+    val workspace: WorkspaceReference
+) : ProjectReference(projectName, url) {
+
+    @Serializer(forClass = Project::class)
+    companion object : INetwork<Project> {
+        override val descriptor: SerialDescriptor = SerialDescriptor("Project") {
+            element<Date>("deliveryTime")
+            element<String>("description")
+            element<String>("projectName")
+            element<String>("status")
+            element("url", UriSerializer.descriptor)
+            element<WorkspaceReference>("workspace")
+        }
+
+        override fun serialize(encoder: Encoder, value: Project) {
+            val compositeOutput = encoder.beginStructure(descriptor)
+            compositeOutput.encodeSerializableElement(descriptor, 0, DateSerializer, value.deliveryTime)
+            compositeOutput.encodeStringElement(descriptor, 1, value.description)
+            compositeOutput.encodeStringElement(descriptor, 2, value.projectName)
+            compositeOutput.encodeStringElement(descriptor, 3, value.status)
+            compositeOutput.encodeSerializableElement(descriptor, 4, UriSerializer, value.url)
+            compositeOutput.encodeSerializableElement(
+                descriptor,
+                5,
+                WorkspaceReference.serializer(),
+                value.workspace
+            )
+            compositeOutput.endStructure(descriptor)
+        }
+
+        override fun deserialize(decoder: Decoder): Project {
+            val dec: CompositeDecoder = decoder.beginStructure(descriptor)
+            var deliveryTime: Date? = null
+            var description: String? = null
+            var projectName: String? = null
+            var status: String? = null
+            var url: Uri? = null
+            var workspace: WorkspaceReference? = null
+            loop@ while(true) {
+                when(val i = dec.decodeElementIndex(descriptor)) {
+                    CompositeDecoder.READ_DONE -> break@loop
+                    0 -> deliveryTime = dec.decodeSerializableElement(descriptor, 0, DateSerializer)
+                    1 -> description = dec.decodeStringElement(descriptor, 1)
+                    2 -> projectName = dec.decodeStringElement(descriptor, 2)
+                    3 -> status = dec.decodeStringElement(descriptor, 3)
+                    4 -> url = dec.decodeSerializableElement(descriptor, 4, UriSerializer)
+                    5 -> workspace = dec.decodeSerializableElement(
+                            descriptor, 5, WorkspaceReference.serializer()
+                    )
+                    else -> throw SerializationException("Unknown index $i")
+                }
+            }
+            dec.endStructure(descriptor)
+            return Project(
+                deliveryTime ?: throw MissingFieldException("deliveryTime"),
+                description ?: throw MissingFieldException("description"),
+                projectName ?: throw MissingFieldException("name"),
+                status ?: throw MissingFieldException("status"),
+                url ?: throw MissingFieldException("url"),
+                workspace ?: throw MissingFieldException("workspace")
+            )
+        }
+    }
+}
---
+++ app/src/main/java/it/uniroma1/keeptime/data/model/ProjectReference.kt
@@ -0,0 +1,17 @@
+package it.uniroma1.keeptime.data.model
+
+import android.net.Uri
+import it.uniroma1.keeptime.data.UriSerializer
+import kotlinx.serialization.Serializable
+
+/**
+ * Base class for projects. It contains only the project's url and its name.
+ */
+@Serializable
+open class ProjectReference(var projectName: String, @Serializable(with = UriSerializer::class) val url: Uri) {
+
+    /**
+     * Retrieves the workspace's attributes from the server.
+     */
+    suspend fun fromServer(): Project = Project.fromServer(url)
+}

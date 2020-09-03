5ea5896a8c67b9286c3e2c00a1b13849ff040137
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-09-04 01:36:48 +0200

    Fix status icon

--- app/src/main/java/it/uniroma1/keeptime/ui/modals/ModalBottomSheet.kt
+++ app/src/main/java/it/uniroma1/keeptime/ui/modals/ModalBottomSheet.kt
@@ -1,19 +1,15 @@
 package it.uniroma1.keeptime.ui.modals
 
-import android.content.Context
 import android.graphics.Color
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.core.content.ContextCompat
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.textview.MaterialTextView
-import java.util.*
-
 import it.uniroma1.keeptime.R
+import java.util.*
 
 
 class ModalBottomSheet(private val description: String, private val status: String?, private val deliveryDate: Date?) : BottomSheetDialogFragment() {
@@ -23,13 +19,17 @@ class ModalBottomSheet(private val description: String, private val status: Stri
         val view = inflater.inflate(R.layout.modal_bottom_sheet, container, false)
         view.findViewById<MaterialTextView>(R.id.modal_bottom_sheet_text).text = description
 
-        val statusColor: Int = when {
-            status == "finished" -> Color.GREEN
-            status == "undefined" -> Color.GRAY
-            deliveryDate?.after(Date()) != null -> Color.RED
-            else -> Color.GREEN
+        if (status != null) {
+            val statusColor: Int = when {
+                status == "finished" -> Color.GREEN
+                status == "undefined" -> Color.GRAY
+                deliveryDate?.after(Date()) != null -> Color.RED
+                else -> Color.GREEN
+            }
+            view.findViewById<ImageView>(R.id.modal_bottom_sheet_status).setColorFilter(statusColor)
+        } else {
+            view.findViewById<ImageView>(R.id.modal_bottom_sheet_status).visibility = View.GONE
         }
-        view.findViewById<ImageView>(R.id.modal_bottom_sheet_status).setColorFilter(statusColor)
 
         return view
     }

1c95072430df9a2baaa6af7f91604818e0e5b068
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-09-05 00:17:00 +0200

    Fix ModalBottomSheet constructor

--- app/src/main/java/it/uniroma1/keeptime/ui/modals/ModalBottomSheet.kt
+++ app/src/main/java/it/uniroma1/keeptime/ui/modals/ModalBottomSheet.kt
@@ -23,6 +23,7 @@ class ModalBottomSheet(
     private val workspaceName: String?
 ) : BottomSheetDialogFragment() {
     constructor(description: String) : this(null, null, description, null, null, null)
+    constructor(description: String, status: String, deliveryDate: Date) : this(null, null, description, deliveryDate, status, null)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.modal_bottom_sheet, container, false)
